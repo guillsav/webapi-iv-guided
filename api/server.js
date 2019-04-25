a9c418595447156a063ac03cec92fa891f31eeb6
guillsav <guillsav@gmail.com>
2019-04-25 13:19:22 -0400

    added env file and dotenv package

--- api/server.js
+++ api/server.js
@@ -11,10 +11,10 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    res.status(200).json({messageOfTheDay: process.env.MODT, shoutouts});
   } catch (error) {
     console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+    res.status(500).json({error: 'Cannot retrieve the shoutouts'});
   }
 });
 
@@ -26,7 +26,7 @@ server.post('/', async (req, res) => {
     res.status(201).json(shoutouts);
   } catch (error) {
     console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
+    res.status(500).json({error: 'Cannot add the shoutout'});
   }
 });
 
